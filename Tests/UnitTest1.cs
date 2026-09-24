65679f2748c8ef95656d856a3cab2fb5b37f5d17
OlgaMak <OlgaMak@DESKTOP-NA2DCSK>
2026-09-24 18:18:38 +0300

    Прописанные тесты

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,11 +1,111 @@
-﻿namespace Tests
+﻿using Library;
+using Xunit;
+using System;
+
+namespace Tests
 {
     public class UnitTest1
     {
+        private readonly NightlightController _controller = new();
         [Fact]
-        public void Test1()
+        public void SetBrightnessZero()
         {
+            var state = new NightlightState { isOn = true, Brightness = 50 };
 
+            _controller.SetBrightness(state, 0);
+
+            Assert.Equal(0, state.Brightness);
+            Assert.False(state.isOn);
         }
+
+        [Fact]
+        public void SetBrightnessHundred()
+        {
+            var state = new NightlightState { isOn = true, Brightness = 50 };
+
+            _controller.SetBrightness(state, 100);
+
+            Assert.Equal(100, state.Brightness);
+            Assert.True(state.isOn);
+        }
+
+        [Fact]
+        public void SetBrightness_OutOfRange_More()
+        {
+            var state = new NightlightState { isOn = true, Brightness = 50 };
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => _controller.SetBrightness(state, 101));
+        }
+
+        [Fact]
+        public void SetBrightness_OutOfRange_Less()
+        {
+            var state = new NightlightState { isOn = true, Brightness = 50 };
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => _controller.SetBrightness(state, -1));
+        }
+
+        [Fact]
+        public void SetTimer_Min_DeactivatesTimer()
+        {
+            var state = new NightlightState
+            {
+                isOn = true,
+                TimerMinutes = 30,
+                isTimerActive = true
+            };
+
+            _controller.SetTimer(state, 0);
+
+            Assert.Equal(0, state.TimerMinutes);
+            Assert.False(state.isTimerActive);
+        }
+
+        [Fact]
+        public void SetTimer_Max_ActivatesTimer()
+        {
+            var state = new NightlightState
+            {
+                isOn = true,
+                TimerMinutes = 30,
+                isTimerActive = true
+            };
+
+            _controller.SetTimer(state, 60);
+
+            Assert.Equal(60, state.TimerMinutes);
+            Assert.True(state.isTimerActive);
+        }
+
+        [Fact]
+        public void SetTimer_OutOfRange_More()
+        {
+            var state = new NightlightState
+            {
+                isOn = true,
+                TimerMinutes = 30,
+                isTimerActive = true
+            };
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => _controller.SetTimer(state, 61));
+        }
+
+        [Fact]
+        public void SetTimer_OutOfRange_Less()
+        {
+            var state = new NightlightState
+            {
+                isOn = true,
+                TimerMinutes = 30,
+                isTimerActive = true
+            };
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => _controller.SetTimer(state, -1));
+        }
+
     }
 }
